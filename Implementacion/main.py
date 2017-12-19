0f6789ae2553bb42a88e355044c1f4818d9ea9a1
Pablo <pbaeyens31@gmail.com>
2017-12-19 10:57:16 +0100

    Cambia base de datos a memoria

--- Implementacion/main.py
+++ Implementacion/main.py
@@ -3,7 +3,7 @@
 # Docs: https://docs.python.org/3.6/library/sqlite3.html
 import sqlite3
 
-conn = sqlite3.connect('main.db')
+conn = sqlite3.connect(':memory:')
 c = conn.cursor()
 
 with open("init.sql",'r') as init:

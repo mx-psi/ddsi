bb44688a8089b4c479e768f30fc9878f36cb7e14
M42 <mromang08@gmail.com>
2017-12-24 19:07:03 +0100

    Consulta creadores

--- Implementacion/main.py
+++ Implementacion/main.py
@@ -41,8 +41,14 @@ commands_completer = WordCompleter([
     'Ver-Productos',
     'Ver-Creadores',
     'Añadir-Productos',
+    'Añadir-Premios',
+    'Consultar-Creadores',
     'Salir',
-  ], ignore_case=True)
+  ], ignore_case = True)
+
+def leerEntidad():
+  c.execute("SELECT nombre FROM entidadCreadora")
+  return prompt("Nombre: ", completer=WordCompleter([t[0] for t in c.fetchall()], ignore_case = True))
 
 
 if __name__ == '__main__':
@@ -74,6 +80,43 @@ if __name__ == '__main__':
       prod_inspirado = prompt('Inspirado en: ')
       c.execute('INSERT INTO productoCulturalPadre VALUES (?, ?, ?, ?, ?)',
                 (12,prod_nombre,prod_fecha,prod_tipo,prod_inspirado))
+
+    elif ic == "Añadir-Entidades":
+      # RF-2.1. Añadir entidad creadora.  Esta función registra una
+      # entidad creadora dentro del sistema a partir de su nombre y
+      # tipo.
+      print('Añadiendo una entidad creadora.')
+      ent_nombre = prompt("Nombre: ")
+      ent_tipo = prompt("Tipo: ")
+      c.execute('INSERT INTO entidadCreadora VALUES (?, ?)',
+                (ent_nombre,ent_tipo))
+
+    elif ic == "Añadir-Premios":
+      # RF-2.2. Registrar un premio asociado a la creación de una
+      # entidad creadora.  Esta función añade un premio dada una
+      # entidad creadora que lo recibe y un producto cultural por el
+      # cual lo recibe.
+      print('Añadiendo un premio')
+      prm_entidad = prompt("Entidad que recibe el premio: ")
+      prm_producto = prompt("ID del producto por el que recibe el premio: ")
+      prm_nombre = prompt("Nombre del premio: ")
+      c.execute('INSERT INTO premiadaPor VALUES (?, ?, ?)',
+                (prm_entidad, prm_producto, prm_nombre))
+
+    elif ic == "Consultar-Creadores":
+      # RF-2.3. Consultar una entidad creadora.  Dado el nombre de una
+      # entidad creadora, esta función muestra el nombre, tipo,
+      # productos culturales creados y premios asociados a esas
+      # creaciones, si los hubiere.
+      ent = leerEntidad()
+      c.execute('SELECT * FROM entidadCreadora WHERE nombre=?', (ent,))
+      print(tabulate(c.fetchall(), headers=['Nombre','Tipo']))
+      c.execute(
+        """SELECT rol, idProducto, nombre, tipo, fechaPublicacion 
+        FROM creadoPor, productoCulturalPadre
+        WHERE (idProducto=id AND nombreCreador=?)""", (ent,))
+      print(tabulate(c.fetchall(), headers=['Rol','ID','Nombre', 'Tipo', 'Fecha']))
+
       
     else:
       print('El comando introducido no es válido')

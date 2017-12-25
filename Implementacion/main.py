0be3fc77fa7f3652ff8eba76755166a8418fd850
Pablo <pbaeyens31@gmail.com>
2017-12-25 14:32:29 +0100

    Reescribe sistema de comandos
    Cada módulo tiene un diccionario de comandos llamado "comandos"
    en el que se asocian un nombre a cada método ejecutable.

--- Implementacion/main.py
+++ Implementacion/main.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-# Docs: https://docs.python.org/3.6/library/sqlite3.html
-import sqlite3
-from prompt_toolkit import prompt
-from prompt_toolkit.contrib.completers import WordCompleter
-from prompt_toolkit.shortcuts import prompt
-from tabulate import tabulate
-from populate import *
+import sqlite3 #Interacción base de datos
+from prompt_toolkit import prompt #CLI
+from prompt_toolkit.contrib.completers import WordCompleter #CLI
+from prompt_toolkit.shortcuts import prompt #CLI
+from tabulate import tabulate #Tabulado de datos
+
+from populate import * # Inicialización de la base de datos
 
 # Subsistemas
 import productos
+import entidades
 
 
 def load(conn, filename):
@@ -38,77 +39,30 @@ c.executemany('INSERT INTO usuario VALUES (?,?,?,?,?,?)', usuario)
 c.executemany('INSERT INTO leGusta VALUES (?,?)', leGusta)
 
 
+def ayuda(c):
+  """Muestra posibles comandos"""
+  l = sorted(map(lambda c: [c, comandos[c].__doc__], comandos.keys()))
+  print(tabulate(l))
 
-commands_completer = WordCompleter([
-    'Listar-Productos',
-    'Añadir-Productos',
-    'Ver-Creadores',
-    'Añadir-Premios',
-    'Consultar-Creadores',
-    'Salir',
-  ], ignore_case = True)
+def salir(c):
+  """Termina la ejecución"""
+  raise EOFError
 
-def leerEntidad():
-  c.execute("SELECT nombre FROM entidadCreadora")
-  return prompt("Nombre: ", completer=WordCompleter([t[0] for t in c.fetchall()], ignore_case = True))
-
+comandos = {"Ayuda": ayuda, "Salir": salir}
+comandos.update(productos.comandos)
+comandos.update(entidades.comandos)
+commands_completer = WordCompleter(comandos.keys(), ignore_case = True)
 
 if __name__ == '__main__':
-  quitar = False
-
-  while not quitar:
-    # Bucle de lectura de comandos
-    print('')
-    ic = prompt('Comando: ', completer=commands_completer)
-    print('')
-
-    if ic == "Salir":  quitar = True
-
-    elif ic == "Listar-Productos": productos.list_all(c)
-    elif ic == "Añadir-Productos": productos.add(c)
-
-    elif ic == "Ver-Creadores":
-      c.execute("SELECT * FROM entidadCreadora")
-      print(tabulate(c.fetchall(), headers=['Nombre','Tipo']))
-
-    elif ic == "Añadir-Entidades":
-      # RF-2.1. Añadir entidad creadora.  Esta función registra una
-      # entidad creadora dentro del sistema a partir de su nombre y
-      # tipo.
-      print('Añadiendo una entidad creadora.')
-      ent_nombre = prompt("Nombre: ")
-      ent_tipo = prompt("Tipo: ")
-      c.execute('INSERT INTO entidadCreadora VALUES (?, ?)',
-                (ent_nombre,ent_tipo))
-
-    elif ic == "Añadir-Premios":
-      # RF-2.2. Registrar un premio asociado a la creación de una
-      # entidad creadora.  Esta función añade un premio dada una
-      # entidad creadora que lo recibe y un producto cultural por el
-      # cual lo recibe.
-      print('Añadiendo un premio')
-      prm_entidad = prompt("Entidad que recibe el premio: ")
-      prm_producto = prompt("ID del producto por el que recibe el premio: ")
-      prm_nombre = prompt("Nombre del premio: ")
-      c.execute('INSERT INTO premiadaPor VALUES (?, ?, ?)',
-                (prm_entidad, prm_producto, prm_nombre))
-
-    elif ic == "Consultar-Creadores":
-      # RF-2.3. Consultar una entidad creadora.  Dado el nombre de una
-      # entidad creadora, esta función muestra el nombre, tipo,
-      # productos culturales creados y premios asociados a esas
-      # creaciones, si los hubiere.
-      ent = leerEntidad()
-      c.execute('SELECT * FROM entidadCreadora WHERE nombre=?', (ent,))
-      print(tabulate(c.fetchall(), headers=['Nombre','Tipo']))
-      c.execute(
-        """SELECT rol, idProducto, nombre, tipo, fechaPublicacion
-        FROM creadoPor, productoCulturalPadre
-        WHERE (idProducto=id AND nombreCreador=?)""", (ent,))
-      print(tabulate(c.fetchall(), headers=['Rol','ID','Nombre', 'Tipo', 'Fecha']))
-
-
-    else:
-      print('El comando introducido no es válido')
-
-  conn.close()
+  try:
+    while True:
+      # Bucle de lectura de comandos
+      print('')
+      ic = prompt('Comando: ', completer=commands_completer)
+      print('')
+      if ic in comandos: comandos[ic](c)
+      else: print('Comando no válido. Introduce \"Ayuda\" para ver los posibles comandos')
+  except (KeyboardInterrupt, EOFError):
+    pass
+  finally:
+    conn.close()
--- Implementacion/productos.py
+++ Implementacion/productos.py
@@ -4,6 +4,7 @@ from prompt_toolkit.shortcuts import prompt
 from tabulate import tabulate
 
 def add(c):
+  """Añade un producto"""
   print('Añadiendo un producto cultural.')
   prod_nombre = prompt('Nombre del producto: ')
   prod_fecha = prompt('Fecha de creación: ')
@@ -12,6 +13,24 @@ def add(c):
   c.execute('INSERT INTO productoCulturalPadre VALUES (?, ?, ?, ?, ?)',
             (12,prod_nombre,prod_fecha,prod_tipo,prod_inspirado))
 
-  def list_all(c):
-    c.execute("SELECT * FROM productoCulturalPadre")
-      print(tabulate(c.fetchall(), headers=['Id','Título','Fecha','Tipo','Inspirado en']))
+def list_all(c):
+  """Lista todos los productos"""
+  c.execute("SELECT * FROM productoCulturalPadre")
+  print(tabulate(c.fetchall(), headers=['Id','Título','Fecha','Tipo','Inspirado en']))
+
+def view(c):
+  """Muestra la información asociada a un producto"""
+  pass
+
+def modify(c):
+  """Modifica un producto cultural existente"""
+  pass
+
+def search(c):
+  """Busca un producto cultural"""
+  pass
+
+comandos = {
+  'Listar-Productos': list_all,
+  'Añadir-Productos': add
+}

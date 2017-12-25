7613417a6fef7472f49cb7b621dac1252b453604
M42 <mromang08@gmail.com>
2017-12-25 20:09:49 +0100

    La implementación con las variables aparte no parece funcionar

--- Implementacion/auxiliar.py
+++ Implementacion/auxiliar.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+import sqlite3 # Interacción base de datos
 from prompt_toolkit.shortcuts import prompt
 from tabulate import tabulate
 from prompt_toolkit.contrib.completers import WordCompleter #CLI
 
 def leer(c, tabla, campo, texto):
     """Función auxiliar: lee de la entrada un elemento de la base de datos."""
-    c.execute("SELECT ? FROM ?", (campo, tabla))
+    c.execute("SELECT " + campo + " FROM " + tabla)
     return prompt(texto, completer=WordCompleter([t[0] for t in c.fetchall()], ignore_case = True))

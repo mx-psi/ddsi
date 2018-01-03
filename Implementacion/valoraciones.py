89e18cb2e8aa8854f1895a6b0ac1fb54401a642e
josemam <jmlosvillares@correo.ugr.es>
2018-01-03 19:00:30 +0100

    RF-4.2, RF-4.3
    La presentación es susceptible de ser cambiada.

--- Implementacion/valoraciones.py
+++ Implementacion/valoraciones.py
@@ -21,6 +21,62 @@ def add_valoracion(c):
             (idProd, user, texto, puntos))
 
 
+#####################################
+# RF-4.2                            #
+# Consultar resumen de valoraciones #
+#####################################
+
+def view_resumen_valoraciones(c):
+  """Obtiene un resumen de las valoraciones de un producto cultural"""
+  idProd = leer2(c, "productoCulturalPadre", "id", "ID del producto cultural valorado: ")
+  c.execute("SELECT puntuacion, COUNT(*) FROM valoracionValora WHERE idProducto = ? GROUP BY puntuacion ORDER BY puntuacion", (idProd, ))
+  histograma = c.fetchall()
+  if len(histograma) == 0:
+    print("\nTodavía no hay valoraciones del producto " + idProd)
+    return
+  print_histograma(histograma)
+  
+  c.execute("SELECT ROUND(AVG(puntuacion)*10)/10 FROM valoracionValora WHERE idProducto = ?", (idProd,))
+  print("Puntuación media: " + str(c.fetchall()[0][0]).replace(".", ","))
+
+  print("\nAlgunas valoraciones:\n")
+  c.execute("SELECT nombreUsuario, v.puntuacion, CASE WHEN LENGTH(resena) > 40 THEN substr(resena, 0, 40) || '...' ELSE resena END,COALESCE(sum(p.puntuacion), 0), -COALESCE(sum(1 - p.puntuacion), 0) FROM valoracionValora v LEFT JOIN puntua p on (p.nombreUsuarioValorador = v.nombreUsuario AND p.idProducto = v.idProducto) WHERE v.idProducto = ? GROUP BY nombreUsuario,v.idProducto ORDER BY COALESCE(sum(2*p.puntuacion-1), 0) DESC LIMIT 3;", (idProd,))
+  print(tabulate(c.fetchall(), headers=['Valorador', 'Puntos', 'Reseña', '+', '-']))
+
+def print_histograma(h):
+  l = [0]*5
+  m = 0
+  for t in h:
+    l[t[0]-1] = t[1]
+    m = max(m, t[1])
+
+  lim = min(40, m)
+  print("\n")
+  for p in range(1, 6):
+    print(str(p) + " | " + "▓"*round(l[p-1]*lim/m))
+
+########################
+# RF-4.3               #
+# Consultar valoración #
+########################
+
+def view_valoracion(c):
+  """Visualiza una valoración"""
+  idProd = leer2(c, "productoCulturalPadre", "id", "ID del producto cultural valorado: ")
+  user   = leer2(c, "usuario", "nombreusuario", "Nombre del usuario valorador: ")
+  c.execute("SELECT nombre, nombreUsuario, puntuacion, resena FROM valoracionValora JOIN productoCulturalPadre ON (id=? AND idProducto=id AND nombreUsuario=?)", (idProd, user))
+  res = c.fetchall()
+  if len(res) == 0:
+    print("\n" + user + " no ha valorado el producto " + idProd)
+    return
+
+  print("\nValoración de {producto} por {usuario}: {pts} puntos\nReseña:\n".format(producto = res[0][0], usuario = res[0][1], pts = res[0][2]))
+  print(res[0][3])
+
+  c.execute("SELECT COALESCE(SUM(puntuacion), 0), COALESCE(SUM(1 - puntuacion), 0) FROM puntua WHERE idProducto=? AND nombreUsuarioValorador=?", (idProd, user))
+  print("\nPuntuación de esta valoración:\n")
+  print(tabulate(c.fetchall(), headers=['A favor', 'En contra']))
+
 ##########################
 # RF-4.4.                #
 # Puntuar una valoración #
@@ -56,6 +112,8 @@ def add_reporte(c):
 
 comandos = {
   'Añadir-Valoración':  add_valoracion,
+  'Ver-Resumen-Valoraciones': view_resumen_valoraciones,
+  'Ver-Valoración': view_valoracion,
   'Puntuar-Valoración':  add_puntuacion,
   'Reportar-Valoración':  add_reporte
 }

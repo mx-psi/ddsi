39ffc73c1647a633c51995f654bef32204287385
VíctorCM <vcm94@hotmail.es>
2018-01-05 01:36:02 +0100

    Añadir y lista usuarios

---
+++ Implementacion/usuarios.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from prompt_toolkit.shortcuts import prompt
+from tabulate import tabulate
+from auxiliar import lee_no_vacio
+
+
+#####################################
+# RF-3.1.                           #
+# Alta de usuario		                #
+#####################################
+
+def add_usuario(c):
+  """Añade un usuario"""
+  print('Añadiendo un usuario.')
+  nombreusuario = lee_no_vacio('Nombre de usuario: ')
+  nombrereal = lee_no_vacio('Nombre real: ')
+  localidadorigen  = lee_no_vacio('Localidad de origen: ')
+  correo   = lee_no_vacio('Correo: ')
+  descripcion  = lee_no_vacio('Descripción del usuario: ')
+  password = lee_no_vacio('Contraseña: ')
+
+  c.executemany('INSERT INTO usuario VALUES (?,?,?,?,?,?)',
+[(nombreusuario,nombrereal,localidadorigen,correo,descripcion,password)])
+
+def lista_usuarios(c):
+  """Lista todos los usuarios"""
+  c.execute("SELECT * FROM usuario")
+  print(tabulate(c.fetchall(), headers=['Nombre de usuario','Nombre real','Localidad de origen','Correo','Descripción del usuario','Contraseña'], tablefmt="plain"))
+
+comandos = {
+  'Añadir-Usuario':  add_usuario,
+  'Listar-Usuarios': lista_usuarios 
+}

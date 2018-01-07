89a5b3cfa90b99b47d311d08ec5c98aded6ba2ca
josemam <jmlosvillares@correo.ugr.es>
2018-01-07 19:42:14 +0100

    leer es bueno

--- Implementacion/valoraciones.py
+++ Implementacion/valoraciones.py
@@ -2,7 +2,7 @@
 
 from prompt_toolkit.shortcuts import prompt
 from tabulate import tabulate
-from auxiliar import lee_texto, lee_entero, leer2
+from auxiliar import lee_texto, lee_entero, leer
 
 ###################################################
 # RF-4.1.                                         #
@@ -12,8 +12,8 @@ from auxiliar import lee_texto, lee_entero, leer2
 def add_valoracion(c):
   """Añade una valoración de un producto cultural"""
   print('Añadiendo una valoración de un producto cultural.')
-  idProd = leer2(c, "productoCulturalPadre", "id", "ID del producto cultural valorado: ")
-  user   = leer2(c, "usuario", "nombreusuario", "Nombre del usuario valorador: ")
+  idProd = leer(c, "productoCulturalPadre", "id", "ID del producto cultural valorado: ")[0]
+  user   = leer(c, "usuario", "nombreusuario", "Nombre del usuario valorador: ")[0]
   texto  = lee_texto("Reseña")
   puntos = lee_entero("Puntuación: ")
 
@@ -28,7 +28,7 @@ def add_valoracion(c):
 
 def view_resumen_valoraciones(c):
   """Obtiene un resumen de las valoraciones de un producto cultural"""
-  idProd = leer2(c, "productoCulturalPadre", "id", "ID del producto cultural valorado: ")
+  idProd = leer(c, "productoCulturalPadre", "id", "ID del producto cultural valorado: ")[0]
   c.execute("SELECT puntuacion, COUNT(*) FROM valoracionValora WHERE idProducto = ? GROUP BY puntuacion ORDER BY puntuacion", (idProd, ))
   histograma = c.fetchall()
   if len(histograma) == 0:
@@ -62,8 +62,8 @@ def print_histograma(h):
 
 def view_valoracion(c):
   """Visualiza una valoración"""
-  idProd = leer2(c, "productoCulturalPadre", "id", "ID del producto cultural valorado: ")
-  user   = leer2(c, "usuario", "nombreusuario", "Nombre del usuario valorador: ")
+  idProd = leer(c, "productoCulturalPadre", "id", "ID del producto cultural valorado: ")[0]
+  user   = leer(c, "usuario", "nombreusuario", "Nombre del usuario valorador: ")[0]
   c.execute("SELECT nombre, nombreUsuario, puntuacion, resena FROM valoracionValora JOIN productoCulturalPadre ON (id=? AND idProducto=id AND nombreUsuario=?)", (idProd, user))
   res = c.fetchall()
   if len(res) == 0:
@@ -85,9 +85,9 @@ def view_valoracion(c):
 def add_puntuacion(c):
   """Añade una puntuación a una valoración"""
   print('Puntuando una valoración.')
-  userP  = leer2(c, "usuario", "nombreusuario", "Nombre del usuario puntuador: ")
-  userV  = leer2(c, "usuario", "nombreusuario", "Nombre del usuario valorador: ")
-  idProd = leer2(c, "productoCulturalPadre", "id", "ID del producto cultural valorado: ")
+  userP  = leer(c, "usuario", "nombreusuario", "Nombre del usuario puntuador: ")[0]
+  userV  = leer(c, "usuario", "nombreusuario", "Nombre del usuario valorador: ")[0]
+  idProd = leer(c, "productoCulturalPadre", "id", "ID del producto cultural valorado: ")[0]
   puntos = lee_entero("Puntuación: ")
 
   c.execute('INSERT INTO puntua VALUES (?, ?, ?, ?)',
@@ -102,9 +102,9 @@ def add_puntuacion(c):
 def add_reporte(c):
   """Reporta una valoración"""
   print('Reportando una valoración.')
-  userR  = leer2(c, "usuario", "nombreusuario", "Nombre del usuario reportador: ")
-  userV  = leer2(c, "usuario", "nombreusuario", "Nombre del usuario valorador: ")
-  idProd = leer2(c, "productoCulturalPadre", "id", "ID del producto cultural valorado: ")
+  userR  = leer(c, "usuario", "nombreusuario", "Nombre del usuario reportador: ")[0]
+  userV  = leer(c, "usuario", "nombreusuario", "Nombre del usuario valorador: ")[0]
+  idProd = leer(c, "productoCulturalPadre", "id", "ID del producto cultural valorado: ")[0]
 
   c.execute('INSERT INTO reporta VALUES (?, ?, ?)',
             (userR, userV, idProd))
